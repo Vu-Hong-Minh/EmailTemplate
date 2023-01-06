ea9378d73ec7300de28c393dfb94f4aa0891bb31
minhvh <minhvh0910.it@gmail.com>
2023-01-06 11:37:34 +0700

    commit 5

--- src/models/email_template.py
+++ src/models/email_template.py
@@ -1,5 +1,5 @@
-from src.models.get_data import GetData
 from datetime import datetime
+from src.models.connection_db import *
 
 class EmailTemplates:
 
@@ -14,41 +14,40 @@ class EmailTemplates:
 
     def check_title_email_template(self):
         sql = "Select id from email_template where title =\'{}\'".format(self.title)
-        data = GetData(sql=sql)
-        result = data.get_data()
+        data = ConnDBEmailTemplate()
+        result = data.get_data(sql=sql)
         return result
 
     def get_email_template_by_id(self):
         sql = "Select merchant_id, title, content from email_template inner join employee on employee_id = employee.id where email_template.id = {}".format(self.id)
-        data = GetData(sql=sql)
-        result = data.get_data()
+        data = ConnDBEmailTemplate()
+        result = data.get_data(sql=sql)
         return result
-
 
     def insert_email_template(self):
         sql = "insert into email_template(employee_id, title, content, created_at) values ({}, \'{}\',\'{}\', \'{}\')".format(self.employee_id, self.title, self.content, self.created_at)
-        data = GetData(sql=sql)
-        data.update_data()
+        data = ConnDBEmailTemplate()
+        data.update_data(sql=sql)
 
     def update_email_template(self):
         sql = "Update email_template set title=\'{}\', content=\'{}\', updated_by={}, updated_at=\'{}\' where id={}".format(
             self.title, self.content, self.updated_by, self.updated_at, self.id)
-        data = GetData(sql=sql)
-        data.update_data()
+        data = ConnDBEmailTemplate()
+        data.update_data(sql=sql)
 
     def delete_email_template(self):
         sql = "Delete from email_template where id ={}".format(self.id)
-        data = GetData(sql=sql)
-        data.update_data()
+        data = ConnDBEmailTemplate()
+        data.update_data(sql=sql)
 
     def get_email_template_list(self, merchant_id):
         sql = "select title, content from email_template inner join employee on employee_id = employee.id where merchant_id = {} ".format(merchant_id)
-        data = GetData(sql=sql)
-        result = data.get_data_list()
+        data = ConnDBEmailTemplate()
+        result = data.get_data_list(sql=sql)
         return result
 
     def search_email_template(self, merchant_id, q):
         sql = 'Select title, content from email_template inner join employee on employee_id = employee.id where merchant_id = {} and title like "%{}%"'.format(merchant_id, q)
-        data = GetData(sql=sql)
-        result = data.get_data_list()
+        data = ConnDBEmailTeplate()
+        result = data.get_data_list(sql=sql)
         return result
