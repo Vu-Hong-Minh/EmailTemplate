3d59ee75faf2b1c51b9b90da1ce569c855d1de95
minhvh <minhvh0910.it@gmail.com>
2023-01-06 11:11:13 +0700

    commit 3

--- src/controllers/email_template_service.py
+++ src/controllers/email_template_service.py
@@ -3,100 +3,101 @@ from flask import Blueprint, request, jsonify
 
 email_templates = Blueprint("email_templates", __name__)
 
-@email_templates.route("/v1/email-templates/<int:email_template_id>", methods=['GET', 'PUT', 'DELETE'])
-def RUD_email_template(email_template_id):
-    #Api lấy thông tin mẫu email
-    if request.method == 'GET':
-        email = EmailTemplates(id=email_template_id)
-        result = email.get_email_template_by_id()
-        if result != None:
-            data = {
-                "merchant_id": result[0],
-                "title": result[1],
-                "content": result[2]
-            }
-            return jsonify({
-                "code": 200,
-                "data": data,
-                "internal_message": "Request thành công"
-            })
-        else:
-            return jsonify({
-                "code": 200,
-                "user_message": "Mẫu email không tồn tại",
-                "internal_message": "Request thành công"
-            })
-
-    #Api cập nhật mẫu email
-    elif request.method == 'PUT':
-        data = request.json
-        employee_id = data.get('employee_id')
-        title = data.get('title')
-        content = data.get('content')
-        email = EmailTemplates(id=email_template_id, title=title, content=content, updated_by=employee_id)
-        email.update_email_template()
-        return jsonify({
-            "code": 200,
-            "user_message": "Update mẫu email thành công",
-            "internal_message": "Request thành công"
-        })
-
-    #Api xóa mẫu email
-    elif request.method == 'DELETE':
-        email = EmailTemplates(id=email_template_id)
-        email.delete_email_template()
-        result = email.get_email_template_by_id()
-        if result == None:
-            return jsonify({
-                "code": 200,
-                "user_message": "Xóa mẫu email thành công",
-                "internal_message": "Request thành công"
-            })
-
-@email_templates.route("/v1/email-templates", methods=['GET', 'POST'])
-def email_template():
-    #Api lấy danh sách mẫu email
-    if request.method == 'GET':
-        data = request.json
-        merchant_id = data.get('merchant_id')
-        email = EmailTemplates()
-        result = email.get_email_template_list(merchant_id=merchant_id)
-        data = []
-        for i in result:
-            temp = {
-                "title": i[0],
-                "content": i[1]
-            }
-            data.append(temp)
+#Api lấy thông tin mẫu email
+@email_templates.route("/v1/email-templates/<int:email_template_id>", methods=['GET'])
+def get_email_template_by_id_service(email_template_id):
+    email = EmailTemplates(id=email_template_id)
+    result = email.get_email_template_by_id()
+    if result != None:
+        data = {
+            "merchant_id": result[0],
+            "title": result[1],
+            "content": result[2]
+        }
         return jsonify({
             "code": 200,
             "data": data,
             "internal_message": "Request thành công"
         })
+    else:
+        return jsonify({
+            "code": 200,
+            "user_message": "Mẫu email không tồn tại",
+            "internal_message": "Request thành công"
+        })
 
-    #Api thêm mới mẫu email
-    elif request.method == 'POST':
-        data = request.json
-        employee_id = data.get('employee_id')
-        title = data.get('title')
-        content = data.get('content')
-        email = EmailTemplates(employee_id=employee_id, title=title, content=content)
-        result1 = email.check_title_email_template()
-        if result1 == None:
-            email.insert_email_template()
-            result = email.check_title_email_template()
-            if result != None:
-                return jsonify({
-                    "code": 200,
-                    "user_message": "Thêm mới mẫu email thành công",
-                    "internal_message": "Request thành công"
-                })
-        else:
+#Api cập nhật mẫu email
+@email_templates.route("/v1/email-templates/<int:email_template_id>/edit", methods=['PUT'])
+def update_email_template_service(email_template_id):
+    data = request.json
+    employee_id = data.get('employee_id')
+    title = data.get('title')
+    content = data.get('content')
+    email = EmailTemplates(id=email_template_id, title=title, content=content, updated_by=employee_id)
+    email.update_email_template()
+    return jsonify({
+        "code": 200,
+        "user_message": "Update mẫu email thành công",
+        "internal_message": "Request thành công"
+    })
+
+#Api xóa mẫu email
+@email_templates.route("/v1/email-templates/<int:email_template_id>/delete", methods=['DELETE'])
+def delete_email_template_service(email_template_id):
+    email = EmailTemplates(id=email_template_id)
+    email.delete_email_template()
+    result = email.get_email_template_by_id()
+    if result == None:
+        return jsonify({
+            "code": 200,
+            "user_message": "Xóa mẫu email thành công",
+            "internal_message": "Request thành công"
+        })
+
+#Api lấy danh sách mẫu email
+@email_templates.route("/v1/email-templates", methods=['GET'])
+def get_email_template_list_service():
+    data = request.json
+    merchant_id = data.get('merchant_id')
+    email = EmailTemplates()
+    result = email.get_email_template_list(merchant_id=merchant_id)
+    data = []
+    for i in result:
+        temp = {
+            "title": i[0],
+            "content": i[1]
+        }
+        data.append(temp)
+    return jsonify({
+        "code": 200,
+        "data": data,
+        "internal_message": "Request thành công"
+    })
+
+#Api thêm mới mẫu email
+@email_templates.route("/v1/email-templates/add", methods=['POST'])
+def insert_email_template_service():
+    data = request.json
+    employee_id = data.get('employee_id')
+    title = data.get('title')
+    content = data.get('content')
+    email = EmailTemplates(employee_id=employee_id, title=title, content=content)
+    result1 = email.check_title_email_template()
+    if result1 == None:
+        email.insert_email_template()
+        result = email.check_title_email_template()
+        if result != None:
             return jsonify({
                 "code": 200,
-                "user_message": "Tiêu đề mẫu email của bạn đã tồn tại, vui lòng đạt tiêu đề khác!",
+                "user_message": "Thêm mới mẫu email thành công",
                 "internal_message": "Request thành công"
             })
+    else:
+        return jsonify({
+            "code": 200,
+            "user_message": "Tiêu đề mẫu email của bạn đã tồn tại, vui lòng đặt tiêu đề khác!",
+            "internal_message": "Request thành công"
+        })
 
 #Api tìm kiếm mẫu email
 @email_templates.route("/v1/email-template/search", methods=['GET'])
@@ -121,7 +122,7 @@ def search_email_template_service():
 
 #Api Copy mẫu email
 @email_templates.route("/v1/email-template/copy", methods=['POST'])
-def copy_email_template():
+def copy_email_template_service():
     data = request.json
     email_template_id = data.get('email_template_id')
     employee_id = data.get('employee_id')
--- src/models/connection_db.py
+++ src/models/connection_db.py
@@ -1,12 +1,52 @@
 import mysql.connector
-# Kết nối vào database.
+# Kết nối tới MySQL.
 class conn_SQL:
-    def __init__(self, host='localhost', db='email_template', user='root', password='123456@Aabc'):
+    def __init__(self, host, db, user, password):
         self.host = host
         self.db = db
         self.user = user
         self.password = password
+        self.connection = mysql.connector.connect(host=self.host, database=self.db, user=self.user, password=self.password)
 
-    def connection_db(self):
-        mydb = mysql.connector.connect(host=self.host, database=self.db, user=self.user, password=self.password)
-        return mydb
+    #Lấy 1 bản ghi
+    def get_data(self, sql):
+        try:
+            cur = self.connection.cursor()
+            cur.execute(sql)
+            result = cur.fetchone()
+            return result
+
+        finally:
+            # Đóng kết nối (Close connection).
+            cur.close()
+            self.connection.close()
+
+    #Lấy nhiều bản ghi
+    def get_data_list(self, sql):
+        try:
+            cur = self.connection.cursor()
+            cur.execute(sql)
+            result = cur.fetchall()
+            return result
+
+        finally:
+            # Đóng kết nối (Close connection).
+            cur.close()
+            self.connection.close()
+
+    #Cập nhật dữ liệu
+    def update_data(self, sql):
+        try:
+            cur = self.connection.cursor()
+            cur.execute(sql)
+            self.connection.commit()
+
+        finally:
+            # Đóng kết nối (Close connection).
+            cur.close()
+            self.connection.close()
+
+# Kết nối đến db email_template
+class conn_SQL_email_teplate(conn_SQL):
+    def __init__(self, host='localhost', db='email_template', user='root', password='123456@Aabc'):
+        super().__init__(host, db, user, password)
